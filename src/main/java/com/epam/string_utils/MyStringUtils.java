90f7dbecc5f8b5105bb51dbd8c12dc19b0820c16
Andriy <andriymattt@gmail.com>
2019-12-01 21:11:48 +0200

    feature/string_utils: method concatenate() method to class MyStringUtils

--- src/main/java/com/epam/string_utils/MyStringUtils.java
+++ src/main/java/com/epam/string_utils/MyStringUtils.java
@@ -2,5 +2,12 @@ package com.epam.string_utils;
 
 public class MyStringUtils {
 
+    static String concatenate(Object... arg) {
+        StringBuilder builder = new StringBuilder();
+        for (Object i : arg)
+            builder.append(i);
+        return builder.toString();
+    }
     
+
 }
