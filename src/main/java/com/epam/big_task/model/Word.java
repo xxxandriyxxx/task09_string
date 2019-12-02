389e9f56a34f082540d30e734cacd8539acda1c4
Andriy <andriymattt@gmail.com>
2019-12-02 10:09:06 +0200

    feature/big_task: add Word class

---
+++ src/main/java/com/epam/big_task/model/Word.java
@@ -0,0 +1,42 @@
+package com.epam.big_task.model;
+
+import java.util.Objects;
+
+public class Word {
+
+    private String value;
+
+    public Word(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Word word = (Word) o;
+        return Objects.equals(value, word.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
+    @Override
+    public String toString() {
+        return "Word{" +
+                "value='" + value + '\'' +
+                '}';
+    }
+
+
+}
