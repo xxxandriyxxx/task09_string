162ba3fb40e5220e158fe7b0a3c578d1e0c98185
Andriy <andriymattt@gmail.com>
2019-12-03 00:30:32 +0200

    feature/big_task: add Main class with entry point

---
+++ src/main/java/com/epam/big_task/Main.java
@@ -0,0 +1,12 @@
+package com.epam.big_task;
+
+import com.epam.big_task.View.View;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        new View().show();
+    }
+}
