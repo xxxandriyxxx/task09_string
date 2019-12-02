156d67617ecd573bb4f0839df33bffbdd7df8bbb
Andriy <andriymattt@gmail.com>
2019-12-02 16:51:03 +0200

    feature/big_task: add sentence sorting method by words amount

--- src/main/java/com/epam/big_task/model/BusinessLogic.java
+++ src/main/java/com/epam/big_task/model/BusinessLogic.java
@@ -1,4 +1,19 @@
 package com.epam.big_task.model;
 
+import java.util.*;
+
 public class BusinessLogic {
+
+    public List<Sentence> getSortedSentenceList(List<Sentence> sentences) {
+        List<Sentence> sortedList = new ArrayList<>(sentences);
+        Collections.sort(sortedList);
+        return sortedList;
+    }
+
+
+
 }
+
+
+
+
