ee67c08cb9436e5ac880c1a2909f083f7dd093b3
Andriy <andriymattt@gmail.com>
2019-12-04 01:10:13 +0200

    feature/i18n: add variables and methods for i18n to View class

--- src/main/java/com/epam/big_task/View/View.java
+++ src/main/java/com/epam/big_task/View/View.java
@@ -13,37 +13,75 @@ public class View {
     private Map<String, String> menu;
     private Map<String, Printable> methodsMenu;
     private static Scanner input = new Scanner(System.in);
+    private Locale locale;
+    private ResourceBundle bundle;
+
+
 
     public View() throws IOException {
         controller = new ControllerImpl();
-        menu = new LinkedHashMap<>();
-        menu.put("1", " 1 - Вивести всі речення.");
-        menu.put("2", " 2 - Знайти найбільшу кількість речень тексту, в яких є однакові слова.");
-        menu.put("3", " 3 - Вивести всі речення заданого тексту у порядку зростання кількості слів у\n" +
-                "кожному з них");
-        menu.put("4", " 4 - Знайти таке слово у першому реченні, якого немає ні в одному з інших\n" +
-                "речень(список слів)");
-        menu.put("5", " 5 - У всіх запитальних реченнях тексту знайти і надрукувати без повторів\n" +
-                "слова заданої довжини.");
-        menu.put("6", " 6 - У кожному реченні тексту поміняти місцями перше слово, що починається\n" +
-                "на голосну букву з найдовшим словом");
-        menu.put("7", " 7 - Надрукувати слова тексту в алфавітному порядку по першій букві. Слова,\n" +
-                "що починаються з нової букви, друкувати з абзацного відступу");
-        menu.put("8", " 8 - З тексту видалити всі слова заданої довжини, що починаються на\n" +
-                "приголосну букву");
-        menu.put("Q", " q - exit");
+        locale = new Locale("uk");
+        bundle = ResourceBundle.getBundle("MyMenu",locale);
+        setMenu();
         methodsMenu = new LinkedHashMap<>();
-        methodsMenu.put("1", this::pressButton1);
-        methodsMenu.put("2", this::pressButton2);
+        methodsMenu.put("1", this::internationalizeMenuUkr);
+        methodsMenu.put("2", this::internationalizeMenuEn);
         methodsMenu.put("3", this::pressButton3);
         methodsMenu.put("4", this::pressButton4);
         methodsMenu.put("5", this::pressButton5);
         methodsMenu.put("6", this::pressButton6);
         methodsMenu.put("7", this::pressButton7);
         methodsMenu.put("8", this::pressButton8);
+        methodsMenu.put("9", this::pressButton9);
+        methodsMenu.put("10", this::pressButton10);
     }
 
-    private void pressButton1() {
+    private void setMenu(){
+        menu = new LinkedHashMap<>();
+        menu.put("1", bundle.getString("1"));
+        menu.put("2", bundle.getString("2"));
+        menu.put("3", bundle.getString("3"));
+        menu.put("4", bundle.getString("4"));
+        menu.put("5", bundle.getString("5"));
+        menu.put("6", bundle.getString("6"));
+        menu.put("7", bundle.getString("7"));
+        menu.put("8", bundle.getString("8"));
+        menu.put("9", bundle.getString("9"));
+        menu.put("10", bundle.getString("10"));
+        menu.put("Q", bundle.getString("Q"));
+//        menu.put("1", " 1 - Вивести всі речення.");
+//        menu.put("2", " 2 - Знайти найбільшу кількість речень тексту, в яких є однакові слова.");
+//        menu.put("3", " 3 - Вивести всі речення заданого тексту у порядку зростання кількості слів у\n" +
+//                "кожному з них");
+//        menu.put("4", " 4 - Знайти таке слово у першому реченні, якого немає ні в одному з інших\n" +
+//                "речень(список слів)");
+//        menu.put("5", " 5 - У всіх запитальних реченнях тексту знайти і надрукувати без повторів\n" +
+//                "слова заданої довжини.");
+//        menu.put("6", " 6 - У кожному реченні тексту поміняти місцями перше слово, що починається\n" +
+//                "на голосну букву з найдовшим словом");
+//        menu.put("7", " 7 - Надрукувати слова тексту в алфавітному порядку по першій букві. Слова,\n" +
+//                "що починаються з нової букви, друкувати з абзацного відступу");
+//        menu.put("8", " 8 - З тексту видалити всі слова заданої довжини, що починаються на\n" +
+//                "приголосну букву");
+//        menu.put("Q", " q - exit");
+    }
+
+
+    private void internationalizeMenuUkr(){
+        locale = new Locale("uk");
+        bundle = ResourceBundle.getBundle("MyMenu",locale);
+        setMenu();
+        show();
+    }
+
+    private void internationalizeMenuEn(){
+        locale = new Locale("en");
+        bundle = ResourceBundle.getBundle("MyMenu",locale);
+        setMenu();
+        show();
+    }
+
+    private void pressButton3() {
         List<Sentence> sentences = new ArrayList<>(controller.getSentences());
         System.out.println("\n========================= all sentences =============================");
         for (Sentence s : sentences) {
@@ -51,7 +89,7 @@ public class View {
         }
     }
 
-    private void pressButton2() {
+    private void pressButton4() {
         System.out.println("\n=================== with most repeated words ==========================");
         Map<Sentence,Integer> sortedByRepeating = controller.getSortedByRepeating();
 
@@ -66,7 +104,7 @@ public class View {
         }
     }
 
-    private void pressButton3() {
+    private void pressButton5() {
         List<Sentence> sortedList = controller.getSortedSentenceList();
         System.out.println("\n========================= sorted sentences =============================");
         for (Sentence s : sortedList) {
@@ -74,13 +112,13 @@ public class View {
         }
     }
 
-    private void pressButton4() {
+    private void pressButton6() {
         List<Word> uniqueWords = controller.getUniqueWords();
         System.out.println("\n===================== unique words in 1st sentence ========================");
         System.out.println(uniqueWords.toString());
     }
 
-    private void pressButton5() {
+    private void pressButton7() {
         System.out.println("\n============= words of a given length of question sentences =====================");
         Map<Sentence, List<Word>> wordMap = controller.getWordsOfQuestion(5);
         for (Map.Entry<Sentence, List<Word>> entry : wordMap.entrySet()) {
@@ -88,7 +126,7 @@ public class View {
         }
     }
 
-    private void pressButton6() {
+    private void pressButton8() {
         controller.replaceLongestVowel();
         System.out.println("\n========================== replace longest and vowel =============================");
         for (Sentence s : controller.getSentences()) {
@@ -96,12 +134,12 @@ public class View {
         }
     }
 
-    private void pressButton7() {
+    private void pressButton9() {
         System.out.println("\n========================== sorted words =============================");
         controller.printWordsByAlphabet();
     }
 
-    private void pressButton8() {
+    private void pressButton10() {
         System.out.println("\n========================== delete word consonant =============================");
         controller.deleteWordsStartConsonant(6);
         for (Sentence s : controller.getSentences()) {
